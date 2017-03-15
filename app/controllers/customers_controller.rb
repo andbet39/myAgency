0efb0a499f1fd0a249d9fea9241d75bca0ef30a8
andrea.terzani <Luglio2015>
2017-03-15 10:20:50 +0100

    customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -28,7 +28,7 @@ class CustomersController < ApplicationController
 
     respond_to do |format|
       if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.html { redirect_to edit_customer_path(@customer.id), notice: 'Contatto creato correttamente.' }
         format.json { render :show, status: :created, location: @customer }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class CustomersController < ApplicationController
   def update
     respond_to do |format|
       if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.html { redirect_to edit_customer_path(@customer.id), notice: 'Contatto aggiornato correttamente.' }
         format.json { render :show, status: :ok, location: @customer }
       else
         format.html { render :edit }
